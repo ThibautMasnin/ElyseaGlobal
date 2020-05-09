bcc7923fa5acc3c8218f1ce501486e6e5f6ac035
Thibaut <thibautmasnin@outlook.fr>
2020-05-09 17:00:20 +0200

    Mise en commun des plugins

--- fr/noctambul/elyseaplugin/PluginListener.java
+++ fr/noctambul/elyseaplugin/PluginListener.java
@@ -15,6 +15,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -27,8 +28,10 @@ import org.bukkit.potion.PotionEffectType;
 
 public class PluginListener implements Listener {
 	
-	
-// LOOT D'OS SUR LES ANIMAUX ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// LOOT D'OS SUR LES ANIMAUX /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	@EventHandler
 	public void onAnimalDeath(EntityDeathEvent e) {
 		Entity mob = e.getEntity();
@@ -45,7 +48,9 @@ public class PluginListener implements Listener {
 	
 	
 
-// EFFETS DE POTION AU RESPAWN ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// EFFETS DE POTION AU RESPAWN ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	@EventHandler
 	public void onRespawn(PlayerRespawnEvent e) {
 		Player p = e.getPlayer();
@@ -60,8 +65,10 @@ public class PluginListener implements Listener {
 	}
 	
 	
-	
-// LOOT MINERAI DE DIAMANT //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// LOOT MINERAI DE DIAMANT ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	@EventHandler
 	public void onBreak(BlockBreakEvent event) {
 		Block b = event.getBlock();
@@ -73,8 +80,10 @@ public class PluginListener implements Listener {
 	}
 	
 	
-	
-// CHAT RP /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// CHAT RP ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	@EventHandler
 	public void playerChat(AsyncPlayerChatEvent e) {
 		e.setCancelled(true);
@@ -123,7 +132,164 @@ public class PluginListener implements Listener {
 	
 	
 
-// MENU VISITEUR ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// VITESSE AVEC ARMURE ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	@EventHandler
+	 void onSetArmor1(InventoryClickEvent event) {
+		
+		Player player = (Player) event.getWhoClicked();
+		ItemStack helmet = player.getInventory().getHelmet();
+		ItemStack chestPlate = player.getInventory().getChestplate();
+		ItemStack leggings = player.getInventory().getLeggings();
+		ItemStack boots = player.getInventory().getBoots();
+		int oneStack = 0;
+		int pices = 4;
+		if (helmet != null && (helmet.getType() == Material.GOLDEN_HELMET || helmet.getType()  == Material.LEATHER_HELMET || helmet.getType() == Material.CHAINMAIL_HELMET)) {
+			oneStack += 1;
+		}else if(helmet == null) {
+			pices -= 1;
+		}if (chestPlate != null && (chestPlate.getType().equals(Material.GOLDEN_CHESTPLATE) || chestPlate.getType() == Material.LEATHER_CHESTPLATE || chestPlate.getType() == Material.CHAINMAIL_CHESTPLATE)) {
+			oneStack += 1;
+		}else if(chestPlate == null) {
+			pices -= 1;
+		}if (leggings != null && ( leggings.getType() == Material.GOLDEN_LEGGINGS || leggings.getType() == Material.LEATHER_LEGGINGS || leggings.getType() == Material.CHAINMAIL_LEGGINGS)) {
+			oneStack += 1;
+		}else if(leggings == null) {
+			pices -= 1;
+		}if (boots != null && ( boots.getType().equals(Material.GOLDEN_BOOTS) || boots.getType().equals(Material.LEATHER_BOOTS) ||boots.getType().equals(Material.CHAINMAIL_BOOTS))) {
+			oneStack += 1;
+		}else if(boots == null) {
+			pices -= 1;
+		}
+		if(pices == 0) {
+			player.removePotionEffect(PotionEffectType.SLOW);
+			
+		}else if(pices == oneStack) {
+			player.removePotionEffect(PotionEffectType.SLOW);
+		}else {
+			player.removePotionEffect(PotionEffectType.SLOW);
+			player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW,Integer.MAX_VALUE,0));
+		}		
+	}	
+	
+	@EventHandler
+	void onSetArmor2(PlayerInteractEvent event) {
+		Player player = (Player) event.getPlayer();
+		ItemStack helmet = player.getInventory().getHelmet();
+		ItemStack chestPlate = player.getInventory().getChestplate();
+		ItemStack leggings = player.getInventory().getLeggings();
+		ItemStack boots = player.getInventory().getBoots();
+		int oneStack = 0;
+		int pices = 4;
+		if (helmet != null && (helmet.getType() == Material.GOLDEN_HELMET || helmet.getType()  == Material.LEATHER_HELMET || helmet.getType() == Material.CHAINMAIL_HELMET)) {
+			oneStack += 1;
+		}else if(helmet == null) {
+			pices -= 1;
+		}if (chestPlate != null && (chestPlate.getType().equals(Material.GOLDEN_CHESTPLATE) || chestPlate.getType() == Material.LEATHER_CHESTPLATE || chestPlate.getType() == Material.CHAINMAIL_CHESTPLATE)) {
+			oneStack += 1;
+		}else if(chestPlate == null) {
+			pices -= 1;
+		}if (leggings != null && ( leggings.getType() == Material.GOLDEN_LEGGINGS || leggings.getType() == Material.LEATHER_LEGGINGS || leggings.getType() == Material.CHAINMAIL_LEGGINGS)) {
+			oneStack += 1;
+		}else if(leggings == null) {
+			pices -= 1;
+		}if (boots != null && ( boots.getType().equals(Material.GOLDEN_BOOTS) || boots.getType().equals(Material.LEATHER_BOOTS) ||boots.getType().equals(Material.CHAINMAIL_BOOTS))) {
+			oneStack += 1;
+		}else if(boots == null) {
+			pices -= 1;
+		}
+		if(pices == 0) {
+			player.removePotionEffect(PotionEffectType.SLOW);
+			
+		}else if(pices == oneStack) {
+			player.removePotionEffect(PotionEffectType.SLOW);
+		}else {
+			player.removePotionEffect(PotionEffectType.SLOW);
+			player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW,Integer.MAX_VALUE,0));
+		}
+	
+	}
+	
+	@EventHandler
+	 void onSetArmor3(InventoryCloseEvent event) {
+		
+		Player player = (Player) event.getPlayer();
+		ItemStack helmet = player.getInventory().getHelmet();
+		ItemStack chestPlate = player.getInventory().getChestplate();
+		ItemStack leggings = player.getInventory().getLeggings();
+		ItemStack boots = player.getInventory().getBoots();
+		int oneStack = 0;
+		int pices = 4;
+		if (helmet != null && (helmet.getType() == Material.GOLDEN_HELMET || helmet.getType()  == Material.LEATHER_HELMET || helmet.getType() == Material.CHAINMAIL_HELMET)) {
+			oneStack += 1;
+		}else if(helmet == null) {
+			pices -= 1;
+		}if (chestPlate != null && (chestPlate.getType().equals(Material.GOLDEN_CHESTPLATE) || chestPlate.getType() == Material.LEATHER_CHESTPLATE || chestPlate.getType() == Material.CHAINMAIL_CHESTPLATE)) {
+			oneStack += 1;
+		}else if(chestPlate == null) {
+			pices -= 1;
+		}if (leggings != null && ( leggings.getType() == Material.GOLDEN_LEGGINGS || leggings.getType() == Material.LEATHER_LEGGINGS || leggings.getType() == Material.CHAINMAIL_LEGGINGS)) {
+			oneStack += 1;
+		}else if(leggings == null) {
+			pices -= 1;
+		}if (boots != null && ( boots.getType().equals(Material.GOLDEN_BOOTS) || boots.getType().equals(Material.LEATHER_BOOTS) ||boots.getType().equals(Material.CHAINMAIL_BOOTS))) {
+			oneStack += 1;
+		}else if(boots == null) {
+			pices -= 1;
+		}
+		if(pices == 0) {
+			player.removePotionEffect(PotionEffectType.SLOW);
+			
+		}else if(pices == oneStack) {
+			player.removePotionEffect(PotionEffectType.SLOW);
+		}else {
+			player.removePotionEffect(PotionEffectType.SLOW);
+			player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW,Integer.MAX_VALUE,0));
+		}		
+	}
+	 void onSetArmor4(PlayerRespawnEvent event) {
+			
+		 	Player player = (Player) event.getPlayer();
+		 	player.sendMessage("respawn");
+		 	ItemStack helmet = player.getInventory().getHelmet();
+			ItemStack chestPlate = player.getInventory().getChestplate();
+			ItemStack leggings = player.getInventory().getLeggings();
+			ItemStack boots = player.getInventory().getBoots();
+			int oneStack = 0;
+			int pices = 4;
+			if (helmet != null && (helmet.getType() == Material.GOLDEN_HELMET || helmet.getType()  == Material.LEATHER_HELMET || helmet.getType() == Material.CHAINMAIL_HELMET)) {
+				oneStack += 1;
+			}else if(helmet == null) {
+				pices -= 1;
+			}if (chestPlate != null && (chestPlate.getType().equals(Material.GOLDEN_CHESTPLATE) || chestPlate.getType() == Material.LEATHER_CHESTPLATE || chestPlate.getType() == Material.CHAINMAIL_CHESTPLATE)) {
+				oneStack += 1;
+			}else if(chestPlate == null) {
+				pices -= 1;
+			}if (leggings != null && ( leggings.getType() == Material.GOLDEN_LEGGINGS || leggings.getType() == Material.LEATHER_LEGGINGS || leggings.getType() == Material.CHAINMAIL_LEGGINGS)) {
+				oneStack += 1;
+			}else if(leggings == null) {
+				pices -= 1;
+			}if (boots != null && ( boots.getType().equals(Material.GOLDEN_BOOTS) || boots.getType().equals(Material.LEATHER_BOOTS) ||boots.getType().equals(Material.CHAINMAIL_BOOTS))) {
+				oneStack += 1;
+			}else if(boots == null) {
+				pices -= 1;
+			}
+			if(pices == 0) {
+				player.removePotionEffect(PotionEffectType.SLOW);
+				
+			}else if(pices == oneStack) {
+				player.removePotionEffect(PotionEffectType.SLOW);
+			}else {
+				player.removePotionEffect(PotionEffectType.SLOW);
+				player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW,Integer.MAX_VALUE,0));
+			}		
+		}
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// MENU VISITEUR /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	@EventHandler
 	public void onJoin(PlayerJoinEvent event) {
 		Player p = event.getPlayer();
